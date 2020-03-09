5babffc2b648148160dbd3b64b45450fceff2550
Lilscorp <iris.lilscorp@gmail.com>
2020-03-09 04:27:13 +0300

    huge update

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+// import {withRouter} from 'react-router-dom';
 
 const burger = (props) => {
+  console.log(props);
   let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
@@ -12,7 +14,6 @@ const burger = (props) => {
     .reduce((arr, el) => {
       return arr.concat(el)
     }, [] );
-  console.log(transformedIngredients);
   if (transformedIngredients.length === 0) {
     transformedIngredients = <p>Please start adding ingredients!</p>;
   }
