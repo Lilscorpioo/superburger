d02e2013f52281524a28c097d1c8c9d282d11456
Lilscorp <iris.lilscorp@gmail.com>
2020-05-07 05:37:22 +0300

    added redirecting after logout

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,13 +3,13 @@ import NavigationItem from './NavigationItem/NavigationItem';
 import classes from './NavigationItems.module.css';
 import { checkPropTypes } from 'prop-types';
 
-const navigationItems = () => (
+const navigationItems = (props) => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/" exact>
       Burger Builder
     </NavigationItem>
     <NavigationItem link="/orders">Orders</NavigationItem>
-    {!checkPropTypes.isAuthenticated ? (
+    {!props.isAuthenticated ? (
       <NavigationItem link="/auth">Authenticate</NavigationItem>
     ) : (
       <NavigationItem link="/logout">Logout</NavigationItem>
